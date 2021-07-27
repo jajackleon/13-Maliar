e1dc262877bc2b51f311ef6dcb0da0e5440d8071
Nicholas <nichz123@gmail.com>
2021-07-27 22:29:17 +0700

    Initial Commit

---
+++ Maliar/MaliarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaliarApp.swift
+//  Maliar
+//
+//  Created by Nicholas on 27/07/21.
+//
+
+import SwiftUI
+
+@main
+struct MaliarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
